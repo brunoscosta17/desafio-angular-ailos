cc479d52de2a09b146319988d1fd1c3e4fc5522e
Costa <booa@gft.com>
2023-03-19 22:59:40 -0300

    criado progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full' },
-  // { path: '', component: CpfQueryComponent },
-  // { path: '', loadChildren: () => import('./modules/cpf-query/cpf-query.module').then(m => m.CpfQueryModule) }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
