650c7c9d6291eeb9b41f66f89d076c3bdd6771b8
Costa <booa@gft.com>
2023-03-20 00:31:27 -0300

    card situação cpf

--- src/app/modules/cpf-query/cpf-query.component.ts
+++ src/app/modules/cpf-query/cpf-query.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faCircleCheck } from '@fortawesome/free-regular-svg-icons';
+
 @Component({
   selector: 'app-cpf-query',
   templateUrl: './cpf-query.component.html',
   styleUrls: ['./cpf-query.component.scss']
 })
 export class CpfQueryComponent implements OnInit {
+
+  faUser = faUser;
+  faCircleCheck = faCircleCheck;
+
+  cpfName: string = 'Bruno da Silva Costa';
+  cpfStatus: string = 'Regular';
 
   constructor() { }
 
